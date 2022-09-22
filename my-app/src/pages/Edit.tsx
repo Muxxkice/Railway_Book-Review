5ed997ae589b0aa529123e5ab2ee883b905d4b24
Mao Kurihara <mao.kurihara@gmail.com>
2022-09-22 18:21:45 +0900

    Modalの作成

--- my-app/src/pages/Edit.tsx
+++ my-app/src/pages/Edit.tsx
@@ -1,15 +1,29 @@
-import React, { useEffect, useMemo } from "react";
-import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useState, useMemo } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 // import { RouteComponentProps } from "react-router-dom";
+
 import { useAppSelector, useAppDispatch } from "../store/hooks";
 
 import { BookType } from "../type/ReviewType";
 import { deleteReview, editReview } from "../api/BookApi";
+import Button from "@mui/material/Button";
+import DialogActions from "@mui/material/DialogActions";
 import Header from "../compornent/Header";
+import "./edit.scss";
 
-export const Edit = () => {
+type FetchState = InitialState | LoadingState | SucsessState | FailState;
+type InitialState = undefined;
+type LoadingState = { _tag: "loading" };
+type SucsessState = { _tag: "success"; data: any };
+type FailState = { _tag: "fail" };
+
+const Edit = () => {
+  const [fetchState, setFetchState] = useState<FetchState>(undefined);
+  const [show, setshow] = useState(false);
   const book = useAppSelector((state) => state.posts.detail);
+  const id = book.id;
+  const navigate = useNavigate();
 
   const defaultValues = useMemo(() => {
     return {
@@ -31,20 +45,68 @@ export const Edit = () => {
     reset(defaultValues);
   }, [defaultValues]);
 
-  const id1 = useParams();
-  const id = useAppSelector((state) => state.posts.id);
   const onSubmit = async (data: BookType) => {
+    setFetchState({ _tag: "loading" });
     console.log(data);
-    editReview(id, data);
+    const res = await editReview(id, data);
+    if (res !== null) {
+      {
+        _tag: "success";
+        data: res;
+      }
+      console.log(res);
+      navigate(`/detail/${id}`);
+    } else {
+      setFetchState({ _tag: "fail" });
+    }
+  };
+
+  const onClickDelete = async () => {
+    setshow(true);
+  };
+
+  const deleteAgree = async () => {
+    setFetchState({ _tag: "loading" });
+    console.log(id);
+
+    const res = await deleteReview(id);
+    if (res === 200) {
+      setshow(false);
+      setFetchState({ _tag: "loading" });
+      console.log(res);
+      navigate("/");
+    } else {
+      console.log("失敗しました");
+    }
+  };
+
+  const Modal = () => {
+    if (show) {
+      return (
+        <div id="modal">
+          <div id="content">
+            <p>本当に削除しますか</p>
+            <button
+              onClick={() => {
+                setshow(false);
+              }}
+            >
+              Cancel
+            </button>
+            <button onClick={deleteAgree}>Agree</button>
+          </div>
+        </div>
+      );
+    }
   };
 
   return (
     <>
       <Header></Header>
-      <h1>Edit</h1>
-      <div className="box">
+      {/* {fetchState === undefined ?():} */}
+      <div className="box" id="overlay">
         <div className="new-container">
-          <h1>書籍の投稿</h1>
+          <h1>書籍の編集</h1>
           <form aria-label="form" onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="title">Title</label>
             <br />
@@ -62,16 +124,18 @@ export const Edit = () => {
             <br />
             <input id="review" {...register("review")} />
             <br />
-            <button type="submit" className="btn">
-              登録
-            </button>
-            <button type="submit" className="btn">
-              削除
+            <button type="submit" className="btn_blue">
+              編集
             </button>
           </form>
+          <button type="submit" className="btn_red" onClick={onClickDelete}>
+            削除
+          </button>
         </div>
+        <Modal />
       </div>
     </>
   );
 };
+
 export default Edit;
