858c4fea2e2d71a5bbcfe17a1b79145b8ebbd4d6
Mao Kurihara <mao.kurihara@gmail.com>
2022-09-29 11:05:20 +0900

    inputの画像を表示

--- my-app/src/pages/Signup.tsx
+++ my-app/src/pages/Signup.tsx
@@ -9,76 +9,91 @@ import {
 import { Link, useNavigate } from "react-router-dom";
 import { signUp, postIcon } from "../api/UserApi";
 import { useAppSelector, useAppDispatch } from "../store/hooks";
-import { userIsAuth, isToken, userToken, setIcon } from "../store/userSlice";
-
+import { userIsAuth, userToken, setIcon } from "../store/userSlice";
+import { MuiSnackbar } from "../compornent/MuiSnackbar";
 import { SiginupUser } from "../type/UserType";
 import "./signup.scss";
 
 export const Signup = () => {
+  // const [authState, setAuthState] = useState(undefined);
+  const [cookies, setCookie] = useCookies<string>(["Token"]); //クッキー
+  const [icon, setIcon] = useState<FormData>();
+  const [isIcon, setIsIcon] = useState<boolean>(false); //画像データがあるか
+  const [errorMessage, setErrorMessage] = useState<string>(); //エラーメッセージ
+  const [previewPath, setPreviewPath] = useState<string>(); //プレビュー用]
+  const [posticonResult, setposticonResult] = useState(false); //iconのポストが成功したか
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<SiginupUser>();
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
-  const [cookies, setCookie] = useCookies<string>(["Token"]);
-
-  const [icon, setIcon] = useState();
-  const [isIcon, setIsIcon] = useState<boolean>(false); //画像データがあるか
-  const [errorMessage, setErrorMessage] = useState<string>(); //エラーメッセージ
-  const [previewPath, setPreviewPath] = useState<string>();
-
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
     const file = acceptedFiles[0];
-    setPreviewPath(file.path);
-    setIcon(file);
+
+    new Compressor(file, {
+      quality: 0.6,
+      async success(result) {
+        const formData = new FormData();
+        console.log(result);
+        formData.append("file", result, "icon");
+        console.log(formData.get("file"));
+        setIcon(formData);
+        setIsIcon(true);
+        const iconurl = URL.createObjectURL(result);
+        setPreviewPath(iconurl);
+      },
+      maxWidth: 300,
+      maxHeight: 300,
+      mimeType: "image/png",
+      error(err) {
+        console.log("画像の圧縮の失敗");
+        console.log(err.message);
+      },
+    });
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-
   const onSubmit = async (data: SiginupUser) => {
     console.log(icon);
-    signup(data, icon);
+    signup(data);
   };
 
-  const signup = async (data: SiginupUser, icon) => {
-    // console.log(icon);
-    const res = await signUp(data);
-    //api叩くタイミング
-    if (res !== null) {
-      setCookie("userToken", res);
+  const signUpwithIcon = async () => {
+    const icon_res = await postIcon(icon);
+    if (icon_res.status === 200) {
+      console.log("画像あり＋signUp成功");
+      console.log(icon_res);
       dispatch(userIsAuth(true));
-      dispatch(userToken(cookies.Token));
-      dispatch(isToken(true));
-      if (cookies !== null && icon !== null) {
-        new Compressor(icon, {
-          quality: 0.6,
-          async success(result) {
-            console.log(result);
-            const formData = new FormData();
-            formData.append("file", result);
-            const icon_res = await postIcon(icon, cookies.Token);
-            //ブラウザ側で対処するか
-            //アップロードした情報がユーザー情報と紐づけられている
-            console.log(icon_res);
-            if (icon_res !== null) {
-              // dispatch(setIcon(icon_res));
-            }
-          },
-          maxWidth: 400,
-          maxHeight: 400,
-          mimeType: "image/jpeg",
-          error(err) {
-            console.log(err.message);
-          },
-        });
-      }
       navigate("/");
+    } else {
+      console.log(icon_res);
+      console.log("画像あり＋signUp失敗(画像部分での失敗)");
+      setposticonResult(false);
     }
   };
+
+  const signup = async (data: SiginupUser) => {
+    const res = await signUp(data);
+
+    if (res !== null) {
+      setCookie("Token", res);
+      dispatch(userToken(res));
+      if (isIcon) {
+        signUpwithIcon();
+      } else if (isIcon === false) {
+        console.log("画像なし＋signUp成功");
+        dispatch(userIsAuth(true));
+        navigate("/");
+      }
+    } else {
+      console.log("signUp失敗(postIcon前での失敗)");
+    }
+  };
+
   return (
     <div className="login-container">
       <h1>Signup</h1>
@@ -111,28 +126,32 @@ export const Signup = () => {
         {errors.password && <span>Password name is required</span>}
         <span>*</span>
         <br />
-        {/* <input type="file" id="icon" onChange={onChangeIcon}></input> */}
+        {/* <input type="file" id="icon" onChange={onChangeIcon}　></input> */}
         <div className="droparia" {...getRootProps()}>
           <input
             type="file"
             id="icon"
-            accept="image/*"
+            accept=".jpg, .png"
             {...getInputProps()}
             // onChange={onChangeIcon}
           ></input>
           <p>Drag 'n' drop a files here, or click to select files</p>
         </div>
         <br />
-
         <button type="submit" className="btn">
           送信
         </button>
       </form>
-
-      <div>
-        {/* <image src={previewPath}></image> */}
-        {previewPath}
-      </div>
+      <p>登録済み</p>
+      <button type="submit">
+        <Link to="/login">Login</Link>
+      </button>
+      {previewPath && (
+        <div key={previewPath} className="preview">
+          <img src={previewPath} />
+        </div>
+      )}
+      <MuiSnackbar />
     </div>
   );
 };
